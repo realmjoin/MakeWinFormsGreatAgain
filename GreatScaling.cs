d418a6455bc0e8c49538b99d7d8bbc65264acdd5
Jan Berdel <jan.berdel@glueckkanja.com>
2019-11-06 14:36:51 +0100

    Add scaling for FormSizeConstraints

--- GreatScaling.cs
+++ GreatScaling.cs
@@ -8,10 +8,14 @@ namespace Mwfga
     {
         private readonly List<(Control control, float factor)> fontScaling = new List<(Control control, float factor)>();
         private readonly Form form;
+        private readonly Size formMinimumSize;
+        private readonly Size formMaximumSize;
 
         public GreatScaling(Form form)
         {
             this.form = form;
+            formMinimumSize = form.MinimumSize;
+            formMaximumSize = form.MaximumSize;
         }
 
         public void PrepareFontScaling(Control control)
@@ -19,7 +23,7 @@ namespace Mwfga
             fontScaling.Add((control, control.Font.SizeInPoints / form.Font.SizeInPoints));
         }
 
-        public void ScaleAfterDpiChange()
+        public void ScaleFontAfterDpiChange()
         {
             foreach (var (control, factor) in fontScaling)
             {
@@ -30,6 +34,21 @@ namespace Mwfga
                     control.Font = new Font(oldFont.FontFamily, factor * form.Font.SizeInPoints, oldFont.Style, GraphicsUnit.Point, oldFont.GdiCharSet, oldFont.GdiVerticalFont);
                 }
             }
+        }
+
+        public void ClearFormSizeConstraintsBeforeDpiChange()
+        {
+            form.MinimumSize = default;
+            form.MaximumSize = default;
+        }
+
+        public void ScaleFormSizeConstraintsAfterDpiChange()
+        {
+            if (formMinimumSize != default)
+                form.MinimumSize = new Size(form.LogicalToDeviceUnits(formMinimumSize.Width), form.LogicalToDeviceUnits(formMinimumSize.Height));
+
+            if (formMaximumSize != default)
+                form.MaximumSize = new Size(form.LogicalToDeviceUnits(formMaximumSize.Width), form.LogicalToDeviceUnits(formMaximumSize.Height));
         }
     }
 }
